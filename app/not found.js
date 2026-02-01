34a28230e984cbd175c3a3dd4af8c0272ca21f45
govindtech-official <contactgovindtech@gmail.com>
2026-02-02 01:49:10 +0530

    Create not found.js

---
+++ app/not found.js
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontFamily: "cursive"
+    }}>
+      <h1>Oops… this page is lost 💔</h1>
+    </div>
+  );
+}
